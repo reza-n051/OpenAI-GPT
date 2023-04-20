00395c2d26b9d6146ef909e7aea28c2f863674a4
reza-n051 <72977248+reza-n051@users.noreply.github.com>
2023-04-21 01:10:43 +0330

    commet for tts

--- server/tts/text_to_speech.py
+++ server/tts/text_to_speech.py
@@ -1,5 +1,12 @@
 from gtts import gTTS
 import requests
+
+#arg1 index       if app fail in converting speech to text it will try a few more times up to the limit(arg2)  (if there was an error)
+#arg2 limit       maximum effort of app for convert text to speech (if there was an error)
+#arg3 text        text that will be converted to speech
+#arg4 lang        the language of the text that will be converted to speech  
+#arg5 file_path   path where speech will be saved
+#arg6 ariana_key  needed for convert persian text to speech
 def text_to_speech(index,limit,text,lang,file_path,ariana_key):
     try:
         if(lang == "fa"):
